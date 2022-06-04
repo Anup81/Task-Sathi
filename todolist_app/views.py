68d6e8b9979286374516d8075e0b954646949743
anup.dhakal <anup.dhakal@ekbana.info>
2022-06-04 18:02:27 +0545

    Using Jinja for making reusable components(Inheritance Method)

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -4,4 +4,21 @@ from django.http import HttpResponse
 
 # Create your views here.
 def todolist(request):
-    return render(request, 'todolist.html', {})
+    context = {
+        'welcome_text': "Welcome to the Todo Page",
+    }
+    return render(request, 'todolist.html', context)
+
+
+def contact(request):
+    context = {
+        'welcome_text': "Welcome to the Contact Page",
+    }
+    return render(request, 'contact.html', context)
+
+
+def about(request):
+    context = {
+        'welcome_text': "Welcome to the About Page",
+    }
+    return render(request, 'about.html', context)
