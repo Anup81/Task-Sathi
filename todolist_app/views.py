38802415a4b09292669849af3dadfc1ed3283af5
Anup81 <anupdhakal81@gmail.com>
2022-06-05 09:17:56 +0545

    Maring task as Completed and Pending Completed

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -24,6 +24,35 @@ def delete_task(request, task_id):
     return redirect('todolist')
 
 
+# For Editing the Task
+def edit_task(request, task_id):
+    if request.method == "POST":
+        task = TaskList.objects.get(pk=task_id)
+        form = TaskForm(request.POST or None, instance=task)
+        if form.is_valid():
+            form.save()
+
+        messages.success(request, ("Task Edited!"))
+        return redirect('todolist')
+    else:
+        task_obj = TaskList.objects.get(pk=task_id)
+        return render(request, 'edit.html', {'task_obj': task_obj})
+
+
+def complete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = True
+    task.save()
+    return redirect('todolist')
+
+
+def pending_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = False
+    task.save()
+    return redirect('todolist')
+
+
 def contact(request):
     context = {
         'contact_text': "Welcome to the Contact Page",
